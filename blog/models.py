7afe91f11bc91aaba4723a2495b8bdd4ea02bd3a
Iv4nGomez <xigomjim596@ieshnosmachado.org>
2025-10-30 11:29:27 +0100

    Add database models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=60)
+    cuerpo = models.TextField()
+    fecha = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"({self.id}){self.titulo}"
--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
 ]
+    
